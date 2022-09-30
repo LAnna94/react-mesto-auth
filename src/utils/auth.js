b4d774b6d6f013225526565a68c78129eaaa4482
LAnna94 <102877411+LAnna94@users.noreply.github.com>
2022-09-30 13:02:33 +0300

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,1 +1,24 @@
+export const BASE_URL = 'https://auth.nomoreparties.co';
 
+export const register = (email, password) => {
+   return fetch(`${BASE_URL}/signup`, {
+       method: 'POST',
+       headers: {
+           "Content-Type": "application/json"
+        },
+       body: JSON.stringify({ email, password })
+    })
+       .then((response) => {
+           try {
+               if (response.status === 200) {
+                   return response.json()
+                }
+            } catch (err) {
+                return (err)
+            }
+        })
+        .then((res) => {
+            return res;
+        })
+        .catch(err => console.log('Ошибка. Запрос не выполнен: ', err));
+}
